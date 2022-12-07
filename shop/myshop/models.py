fca12fb72340118a7b21a449e918f5c87850cb2f
mamroax <mamroax@gmail.com>
2022-12-07 21:38:14 +0300

    +

--- shop/myshop/models.py
+++ shop/myshop/models.py
@@ -22,14 +22,14 @@ class Product(models.Model):
                                  related_name='products',
                                  on_delete=models.CASCADE)
 
-    name = models.CharField(max_length=150, db_index=True)
-    slug = models.CharField(max_length=150, db_index=True, unique=True)
+    name = models.CharField(max_length=150, db_index=True, verbose_name="Наименование")
+    slug = models.CharField(max_length=150, db_index=True, unique=True, verbose_name="Ссылка")
     image = models.ImageField(upload_to='product/%Y/%m/%d', blank=True)
-    description = models.TextField(max_length=1000, blank=True)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    available = models.BooleanField(default=True)
-    created = models.DateTimeField(auto_now_add=True)
-    uploaded = models.DateTimeField(auto_now=True)
+    description = models.TextField(max_length=1000, blank=True, verbose_name="Описание")
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Цена")
+    available = models.BooleanField(default=True, verbose_name="Наличие")
+    created = models.DateTimeField(auto_now_add=True, verbose_name="Добавлен")
+    uploaded = models.DateTimeField(auto_now=True, verbose_name="Изменен")
 
     class Meta:
         ordering = ('name',)
--- shop/shop/settings.py
+++ shop/shop/settings.py
@@ -17,6 +17,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
+
+CART_SESSION_ID = 'cart'
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
@@ -64,6 +66,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'cart.context_processors.cart'
             ],
         },
     },
